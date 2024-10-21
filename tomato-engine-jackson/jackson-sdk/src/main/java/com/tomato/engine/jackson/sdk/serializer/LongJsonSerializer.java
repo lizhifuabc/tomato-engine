0cbdc5eafa6b2575117f91080a1f8995cb78184b
lizhifu <lizhifuabc@163.com>
2024-10-21 09:51:14 +0800

    fix(json): long 序列化

--- tomato-engine-jackson/jackson-sdk/src/main/java/com/tomato/engine/jackson/sdk/serializer/LongJsonSerializer.java
+++ tomato-engine-jackson/jackson-sdk/src/main/java/com/tomato/engine/jackson/sdk/serializer/LongJsonSerializer.java
@@ -16,9 +16,16 @@ public class LongJsonSerializer extends JsonSerializer<Long> {
 
     @Override
     public void serialize(Long value, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
-        String text = (value == null ? null : String.valueOf(value));
-        if (text != null) {
-            jsonGenerator.writeString(text);
+        if (null == value) {
+            jsonGenerator.writeNull();
+            return;
+        }
+        // js中最大安全整数16位 Number.MAX_SAFE_INTEGER
+        String longStr = String.valueOf(value);
+        if (longStr.length() > 16) {
+            jsonGenerator.writeString(longStr);
+        } else {
+            jsonGenerator.writeNumber(value);
         }
     }
 }
