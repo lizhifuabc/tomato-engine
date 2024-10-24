133454f1501a9995f0fe5e8a5c0e178f806f8289
lizhifu <lizhifuabc@163.com>
2024-10-24 17:40:22 +0800

    feat(mybatis): 基于 mybatis ORM 框架 mybatis-mp sql 替换

--- tomato-engine-mybatis/mybatis-sdk/src/main/java/com/tomato/engine/mybatis/sdk/replace/SqlReplace.java
+++ tomato-engine-mybatis/mybatis-sdk/src/main/java/com/tomato/engine/mybatis/sdk/replace/SqlReplace.java
@@ -3,6 +3,7 @@ package com.tomato.engine.mybatis.sdk.replace;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ParameterMap;
 import org.apache.ibatis.mapping.SqlSource;
 import org.apache.ibatis.plugin.Invocation;
 
@@ -41,7 +42,7 @@ public class SqlReplace {
         });
 
         // 把新的查询放到statement里
-        MappedStatement newMs = copyFromMappedStatement(mappedStatement, new BoundSqlSqlSource(newBoundSql));
+        MappedStatement newMs = copyFromMappedStatement(mappedStatement, new BoundSqlSqlSource(newBoundSql),null);
         args[0] = newMs;
     }
 
@@ -51,24 +52,31 @@ public class SqlReplace {
      *
      * @param ms          原MappedStatement对象
      * @param newSqlSource 新的SqlSource对象，用于替换原MappedStatement中的SqlSource
+     * @param parameterMap 新的ParameterMap对象，用于替换原MappedStatement中的ParameterMap，如果为null则使用原MappedStatement的ParameterMap
      * @return 生成的新MappedStatement对象
      */
-    private static MappedStatement copyFromMappedStatement(MappedStatement ms, SqlSource newSqlSource) {
+    public static MappedStatement copyFromMappedStatement(MappedStatement ms, SqlSource newSqlSource, ParameterMap parameterMap) {
         MappedStatement.Builder builder = new MappedStatement.Builder(ms.getConfiguration(), ms.getId(), newSqlSource, ms.getSqlCommandType());
 
         builder.resource(ms.getResource());
         builder.fetchSize(ms.getFetchSize());
         builder.statementType(ms.getStatementType());
         builder.keyGenerator(ms.getKeyGenerator());
+
+        // 处理主键属性
         if (ms.getKeyProperties() != null && ms.getKeyProperties().length > 0) {
             StringBuilder keyPropertiesBuilder = new StringBuilder();
             String[] keyProperties = ms.getKeyProperties();
-            Arrays.stream(keyProperties).forEach(key->keyPropertiesBuilder.append(key).append(","));
+            Arrays.stream(keyProperties).forEach(key -> keyPropertiesBuilder.append(key).append(","));
             keyPropertiesBuilder.delete(keyPropertiesBuilder.length() - 1, keyPropertiesBuilder.length());
             builder.keyProperty(keyPropertiesBuilder.toString());
         }
+
         builder.timeout(ms.getTimeout());
-        builder.parameterMap(ms.getParameterMap());
+
+        // 使用提供的parameterMap，或回退到原始的parameterMap
+        builder.parameterMap(parameterMap != null ? parameterMap : ms.getParameterMap());
+
         builder.resultMaps(ms.getResultMaps());
         builder.resultSetType(ms.getResultSetType());
         builder.cache(ms.getCache());
@@ -77,6 +85,7 @@ public class SqlReplace {
 
         return builder.build();
     }
+
 
 
     private record BoundSqlSqlSource(BoundSql boundSql) implements SqlSource {
